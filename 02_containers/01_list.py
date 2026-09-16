ed3365589bb2f8ff1aece10249cd05da76960848
Zhaozhiming <1766332704@qq.com>
2026-09-16 16:40:30 +0800

    f"this is my first to create branch into githun!"

--- 02_containers/01_list.py
+++ 02_containers/01_list.py
@@ -1,6 +1,6 @@
 # 列表按顺序保存多个值，可以添加、删除和修改。
 # 场景：记录待办事项、成绩或购物清单。
-
+print(f"this is my first to create branch into githun!")
 fruits = ["苹果", "香蕉", "橘子"]
 print(fruits[0])  # 苹果：下标从 0 开始。
 print(fruits[-1])  # 橘子
